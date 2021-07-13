6f66dfba26438110fdd509b9eebde38ba85f474c
Reilly-Oduory <reilly.oduory@student.moringaschool.com>
2021-07-13 10:31:03 +0300

    auth system works

--- insta/urls.py
+++ insta/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     # auth urls
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 
     # user urls
+    path('', views.home, name='home'),
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,21 +1,43 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+
 from .forms import RegistrationForm
 
 # Create your views here.
-def register(request):
+def register_user(request):
     form = RegistrationForm()
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            login(request)
+            messages.success(request, "You have successfully Signed Up")
             return redirect(f'/')
         
     context = {"form":form}
     return render(request, 'auth/register.html', context)
 
-def login(request):
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully logged in")
+            return redirect(f'/')
+        else:
+            messages.info(request, "The Username or Password is incorrect")
+
     context = {}
     return render(request, 'auth/login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('/login')
 
 def home(request):
     context = {}
